7c5e56e74bc13f3373af36713e0f06cfbc9faa49
ZahiraZahra <manuelsipriaganteng@gmail.com>
2026-07-14 19:17:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 
 // 🟢 KONEKSI KE DATABASE CLOUD ONLINE (AIVEN.IO)
 const db = mysql.createConnection({
